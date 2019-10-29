2ec9f52674e1ed2b0775da13fcb212c0c1bb55db
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2019-10-29 09:43:52 +0100

    Use output instead of > to avoid mixing stdout

--- whiscy_protocol.sh
+++ whiscy_protocol.sh
@@ -5,7 +5,7 @@ protein=$1
 whiscy.py $protein.sur $protein.conv $protein.phylseq $protein.out -o $protein.cons
 whiscy.py $protein.lac $protein.conv $protein.phylseq $protein.out -o $protein.lcons
 
-${WHISCY_PATH}/bin/consadjust.py $protein.cons ${WHISCY_PATH}/param/weight_ma.txt ${WHISCY_PATH}/param/ztable.txt > $protein.acons
+${WHISCY_PATH}/bin/consadjust.py $protein.cons ${WHISCY_PATH}/param/weight_ma.txt ${WHISCY_PATH}/param/ztable.txt -o $protein.acons
 
 ${WHISCY_PATH}/bin/residue_distance.py $protein.pdb $protein.conv $protein.rd
 
