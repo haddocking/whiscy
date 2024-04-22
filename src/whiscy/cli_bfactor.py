0a9ab7a3968afface57ae31285df17c06bd835de
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-04-22 11:01:34 +0200

    add whiscy_bfactor script

--- src/whiscy/cli_bfactor.py
+++ src/whiscy/cli_bfactor.py
@@ -8,10 +8,28 @@ import argparse
 import os
 import sys
 
-STANDARD_TYPES = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C',
-                  'GLU': 'E', 'GLN': 'Q', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
-                  'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P',
-                  'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V'}
+STANDARD_TYPES = {
+    "ALA": "A",
+    "ARG": "R",
+    "ASN": "N",
+    "ASP": "D",
+    "CYS": "C",
+    "GLU": "E",
+    "GLN": "Q",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LEU": "L",
+    "LYS": "K",
+    "MET": "M",
+    "PHE": "F",
+    "PRO": "P",
+    "SER": "S",
+    "THR": "T",
+    "TRP": "W",
+    "TYR": "Y",
+    "VAL": "V",
+}
 
 
 def parse_whiscy_scores(file_name):
@@ -27,15 +45,29 @@ def parse_whiscy_scores(file_name):
     return scores
 
 
-if __name__ == "__main__":
+def main():
 
     # Parse command line
-    parser = argparse.ArgumentParser(prog='whiscy2bfactor')
-    parser.add_argument("input_pdb_file", help="Input PDB structure file", metavar="input_pdb_file")
-    parser.add_argument("output_pdb_file", help="Output PDB file", metavar="output_pdb_file")
-    parser.add_argument("whiscy_scores_file", help="WHISCY scores file", metavar="whiscy_scores_file")
-    parser.add_argument("--norm", help="Normalize WHISCY scores", dest="norm", action='store_true', default=False)
-    parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
+    parser = argparse.ArgumentParser(prog="whiscy2bfactor")
+    parser.add_argument(
+        "input_pdb_file", help="Input PDB structure file", metavar="input_pdb_file"
+    )
+    parser.add_argument(
+        "output_pdb_file", help="Output PDB file", metavar="output_pdb_file"
+    )
+    parser.add_argument(
+        "whiscy_scores_file", help="WHISCY scores file", metavar="whiscy_scores_file"
+    )
+    parser.add_argument(
+        "--norm",
+        help="Normalize WHISCY scores",
+        dest="norm",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s {}".format(__version__)
+    )
     args = parser.parse_args()
 
     input_pdb_file_name = args.input_pdb_file
@@ -45,8 +77,8 @@ if __name__ == "__main__":
     # Parse scores from the WHISCY output file
     scores = parse_whiscy_scores(whiscy_scores_file_name)
 
-    with open(input_pdb_file_name, 'r') as input:
-        with open(output_pdb_file_name, 'w') as output:
+    with open(input_pdb_file_name, "r") as input:
+        with open(output_pdb_file_name, "w") as output:
             for line in input:
                 line = line.rstrip(os.linesep)
                 if line and line.startswith("ATOM  "):
@@ -65,4 +97,12 @@ if __name__ == "__main__":
                     line = line[:61] + "%6.2f" % score + line[66:]
                 output.write(line + os.linesep)
 
-    print("{} PDB file with WHISCY scores in B-factor column has been created".format(output_pdb_file_name))
+    print(
+        "{} PDB file with WHISCY scores in B-factor column has been created".format(
+            output_pdb_file_name
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
