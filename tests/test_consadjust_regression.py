012d6132d680d403c3a2a600b03ee7103a5493c1
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-04-22 13:05:22 +0200

    update tests

--- tests/test_consadjust_regression.py
+++ tests/test_consadjust_regression.py
@@ -7,7 +7,6 @@ import os
 import pytest
 
 from . import GOLDEN_DATA_PATH
-from whiscy.modules import PARAM_PATH
 
 env = os.environ.copy()
 env["PYTHONPATH"] = str(
@@ -25,16 +24,6 @@ def cons_file():
 
 
 @pytest.fixture
-def weight_ma_file():
-    return Path(PARAM_PATH, "weight_ma.txt")
-
-
-@pytest.fixture
-def ztable_file():
-    return Path(PARAM_PATH, "ztable.txt")
-
-
-@pytest.fixture
 def acons_file():
     return Path(GOLDEN_DATA_PATH, "regression_consadjust", "2SNIE.acons")
 
@@ -48,12 +37,12 @@ def acons_file():
 @pytest.mark.parametrize(
     "scratch_path", ["scratch_regression_consadjust"], indirect=True
 )
-def test_regression_consadjust2SNIE(
-    scratch_path, cons_file, weight_ma_file, ztable_file, acons_file
-):
+def test_regression_consadjust2SNIE(scratch_path, cons_file, acons_file):
 
     test_prediction_output = Path(scratch_path, "test.prediction")
-    cmd_line = f"{sys.executable} {CONSADJUST_BIN} {cons_file} {weight_ma_file} {ztable_file} -o {test_prediction_output}"
+    cmd_line = (
+        f"{sys.executable} {CONSADJUST_BIN} {cons_file} -o {test_prediction_output}"
+    )
 
     result = subprocess.run(
         cmd_line.split(),
